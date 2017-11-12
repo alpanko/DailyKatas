ddd95b0c19df4466e6d29b8c60007f2e9000ac88
Patrick Nkomo <patrick.nkomo@gmail.com>
2017-11-12 21:43:20 +0100

    gggggg

--- DailyKatas/src/main/java/it/patrick/demo/composite/day1/org/Developer.java
+++ DailyKatas/src/main/java/it/patrick/demo/composite/day1/org/Developer.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package zdpatternkatas.composite.day1.org;
+package it.patrick.demo.composite.day1.org;
 
 /**
  *
--- DailyKatas/src/main/java/it/patrick/demo/composite/day1/org/Employee.java
+++ DailyKatas/src/main/java/it/patrick/demo/composite/day1/org/Employee.java
@@ -3,13 +3,15 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package zdpatternkatas.composite.day1.org;
+package it.patrick.demo.composite.day1.org;
 
 /**
  *
  * @author patrickn
  */
 public interface Employee {
+    
+    
 
    public void add(Employee employee);
    public void remove(Employee employee);
